83ba22fc7bfb12db3f06f3fe083d884e90c921b4
Christopher Hooper <hooper.christopher@gmail.com>
2020-04-28 19:27:35 -0700

    Auth (#3)
    * add key and hide key
    
    * add setup scripts
    
    * add instructions and test out auth

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PoliticalClustering
+
+# install docker and stuff
+```bash
+bash bash_lib/setup.sh
+```
+### But API key in api_data_gov.key
+
+# start development shell
+```bash
+bash bash_lib/shell.sh
+```
+
+# export api key
--- py_lib/core/auth.py
+++ py_lib/core/auth.py
@@ -1,5 +1,22 @@
+import os
+import requests
+
 def hello_world():
     print("hello world.")
 
+
+def search_candidates(api_key):
+    """
+    https://api.open.fec.gov/developers#/candidate/get_candidates_
+    """
+    get_candidates = """https://api.open.fec.gov/v1/candidates/?sort=name&sort_hide_null=false&sort_null_only=false&sort_nulls_last=false&page=1&per_page=20&api_key={api_key}""".format(
+        api_key=api_key
+    )
+    response = requests.get(get_candidates)
+    return response
+
 if __name__ == "__main__":
-    hello_world()
+    with open('/bighead/api_data_gov.key') as f:
+        api_key = f.read()
+        print(search_candidates(api_key=api_key))
+
